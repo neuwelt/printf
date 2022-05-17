c45cdbbe47567a5bd72afb84194fa84e33fc50bb
neuwelt <ineuwelt@neuwelt.co>
2022-05-17 23:21:03 +0000

    Bug fix 7

--- _printf.c
+++ _printf.c
@@ -6,45 +6,110 @@
  *
  * Return: number of bytes printed
  */
-int _printf(const char *format, ...)
+
+char *_strrev (char *str)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+  int i;
+  int len = 0;
+  char c;
+  if (!str)
+    return NULL;
+  while(str[len] != '\0'){
+    len++;
+  }
+  for(i = 0; i < (len/2); i++)
+  {
+    c = str[i];
+    str [i] = str[len - i - 1];
+    str[len - i - 1] = c;
+  }
+  return str;
+}
 
-	va_start(ap, format);
+//For integer to string conversion
+char * _itoa(int i, char *strout, int base)
+{
+  char *str = strout;
+  int digit, sign = 0;
+  if (i < 0) {
+    sign = 1;
+    i *= -1;
+  }
+  while(i) {
+    digit = i % base;
+    *str = (digit > 9) ? ('A' + digit - 10) : '0' + digit;
+    i = i / base;
+    str ++;
+  }
+  if(sign) {
+  *str++ = '-';
+  }
+  *str = '\0';
+  _strrev(strout);
+  return strout;
+}
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
-	{
-		init_params(&params, ap);
-		if (*p != '%')
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
-	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+int _printf(char * str, ...)
+{
+  va_list args;
+  int i = 0, j=0;
+  char buff[100]={0}, tmp[20];
+  char * str_arg;
+  
+  va_start( args, str );
+  while (str && str[i])
+  {
+    if(str[i] == '%'){
+      i++;  //move to next address
+      
+      switch (str[i]) {
+        /* Convert char */
+        case 'c': {
+          buff[j] = (char)va_arg( args, int );
+          j++;
+          break;
+        }
+        
+        /* Convert decimal */
+        case 'd': {
+          _itoa(va_arg( args, int ), tmp, 10);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        
+        /* Convert hex */
+        case 'x': {
+          _itoa(va_arg( args, int ), tmp, 16);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        
+        /* Convert octal */
+        case 'o': {
+          _itoa(va_arg( args, int ), tmp, 8);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        
+        /* copy string */
+        case 's': {
+          str_arg = va_arg( args, char* );
+          strcpy(&buff[j], str_arg);
+          j += strlen(str_arg);
+          break;
+        }
+      }
+    } else {
+      buff[j] = str[i];
+      j++;
+    }
+    i++;
+  } 
+  fwrite(buff, j, 1, stdout); 
+  va_end(args);
+  return j;
 }
 
